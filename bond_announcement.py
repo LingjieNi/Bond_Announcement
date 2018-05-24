937728da7db994619b8aba79fe50697d53fba2f0
Andreas Foitzik <andreas.foitzik@eviarc.com>
2018-05-24 12:24:47 +0200

    Simulating factor values after discretizing Factor's Ornstein-Uhlenbeck Process

--- bond_announcement.py
+++ bond_announcement.py
@@ -12,7 +12,7 @@ matplotlib.style.use('ggplot')
 # ---
 
 # Set the seed to generate a certain set of random numbers
-np.random.seed(12)
+np.random.seed(1)
 nobs        = 1000
 
 # monthly observations, 1 equals one year
@@ -41,19 +41,23 @@ sigma       = np.matrix(([c, 0., 0.], [0., c, 0.], [sigma_31, sigma_32, sigma_33
 # Initial factor values
 # TODO - chose proper initial values
 k      = 3
-x      = np.empty([nobs,k])
-x[0,:] = np.array([0.0042, 0.0021, 0.0028])
+x      = np.zeros([nobs,k])
+x[0,:] = np.matrix([0.0042, 0.0021, 0.0028])
 
 # Constant unconditional variance
+# TODO - check for the right variance
 sigma2 = (sigma**2).dot(np.linalg.inv(K_P) * (1 - np.exp(- 2 * K_P)))  # constant volatility
 
 # Simulation
 for t in range(1,nobs):
-    er = np.array([np.random.normal(0,1,1), np.random.normal(0,1,1), np.random.normal(0,1,1)])
-    x[t] = x[t-1] + K_P.dot(thetaP-x[t-1]) * dt + sigma * np.sqrt(dt) * er
+    error_term1 = np.random.normal(0,1,1)
+    error_term2 = np.random.normal(0,1,1)
+    error_term3 = np.random.normal(0,1,1)
+    error_terms = np.array([error_term1, error_term2, error_term3])
+    x[t, :]     = x[t-1, :] + np.asarray(K_P.dot(thetaP-x[t-1]).T * dt).reshape(-1) + np.asarray(sigma * error_terms).reshape(-1)
 
 plt.figure(2)
-plt.plot(x, label='state variables',  color='#1E90FF')
+plt.plot(x, label='state variables')
 plt.legend(loc='upper left', ncol=1)
 
 # ---
@@ -74,13 +78,19 @@ thetaQ          = np.linalg.inv(K_Q).dot(K_P.dot(thetaP - sigma.dot(trans_lambda
 # ---
 
 # Calculate Vasicek implied bond loadings
-Btau = ( np.linalg.inv(K_Q).dot(1 - np.exp( - K_Q * tau)))
+a = np.empty([len(tau),k])
+b = np.empty([len(tau),k])
 
-Atau = ((thetaQ - sigma * sigma / alphaQ / alphaQ / 2)*(Btau - tau) - sigma * sigma * np.square(Btau) / (4 * alphaQ))
-yields = np.zeros([1000,6])
-for i in range(0,6):
-    for t in range(0,1000):
-        yields[t,i] = (riskfree[t]*Btau[i]/tau[i] - Atau[i]/tau[i])
+for i in range(0,k):
+  for j in range(0,len(tau)):
+    b[j, i] = 1 * np.linalg.inv(K_Q) * (1 - np.exp(- K_Q * tau[j])) / tau[j]
+    b[j,i] = 1 * np.linalg.inv(K_Q) * (1-np.exp( - K_Q * tau[j])) / tau[j]
+    a[j,i] = (thetaQ[i] - sigma[i] * sigma[i] / alphaQ[i] / alphaQ[i] / 2)*(b[j,i]*tau[j] - tau[j]) + sigma[i] * sigma[i] * np.square(b[j,i]*tau[j]) / (4 * alphaQ[i])
+
+yields = b.dot(np.transpose(x)) - (np.sum(a,axis=1)).reshape(m,1) + 0.0001 * np.random.normal(0,1,[np.size(tau),nobs])
+
+# Calculate yields and add measurement error
+#yields = ( rate.dot(Btau.reshape(1,np.size(tau))) + Atau )/tau + 0.0001 * np.random.normal(0,1,[nobs,np.size(tau)])
 
 yields = yields + np.random.normal(0, 0.0001, [nobs, len(tau)])
 
@@ -93,9 +103,3 @@ plt.plot(yields[:,1], label='6 month',color='0.80')
 plt.plot(yields[:,0], label='1 month',color='1.00')
 plt.plot(riskfree, label='inst. rate',  color='#1E90FF')
 plt.legend(loc='upper left', ncol=7)
-
-
-# ---
-# PART - 2
-# Calculate Price
-# ---
